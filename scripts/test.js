de24d786f7f25622b627c8fad1bd1d15ff5662da
Gabriel Viernes <viernesgabriel@gmail.com>
2023-08-27 23:46:04 -0500

    added button functionality via js

---
+++ scripts/test.js
@@ -0,0 +1,14 @@
+    //this creates a new "Audio" object that can be returned
+var pootis = new Audio("./assets/audio/pootis.wav");
+
+function play(){
+
+
+    return pootis;
+
+}
+
+function test() {
+    var a = 5;
+    return a;
+}
