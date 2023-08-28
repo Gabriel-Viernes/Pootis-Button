3d802c716390592522b80b864d65d609ae9f5eee
Gabriel Viernes <viernesgabriel@gmail.com>
2023-08-28 16:59:22 -0500

    rearranged code, renamed scripts to js

---
+++ js/test.js
@@ -0,0 +1,10 @@
+
+    // retrieves audio with id = pootis
+// var pootis = document.getElementById("pootis");
+// function playSound() {
+//     pootis.play();
+// }
+// function increment() {
+// count = document.getElementById("pootisCount").value + 1;
+// document.getElementById("pootisCount").innerHTML = count;
+// }
